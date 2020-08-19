48536d3eeb4be8ce427c19961b2ecb034b89d89c
Anton Plisnier <anttongp@gmail.com>
2020-08-19 17:33:20 +0100

    failed to be able to add multiple groups into the grouped area. I am 100% certain that I do not yet sufficiently understand asynchronisity in Javascript and must come back to it tomorrow. This new branch should not be deleted as It perfectly demostrates what happens when you go AGAINST the framework that's given. Lesson learnt.

--- background.js
+++ background.js
@@ -17,12 +17,30 @@ function extractGroupFromIndex() {
   selectedFromIndex.forEach(function(selection) {
     chrome.tabs.query({index:selection, currentWindow:true}, function(result) {  
       result.forEach(function(filtered) {
-        group += `<input name='groupElement'type='checkbox' value=${filtered.index}><label for='groupElement'>${filtered.title}</label><br>`;     
-      });
-      chrome.storage.sync.set({'grouping':group}, function() {
-        console.log('Saved', 'grouping', group);
+        group  += `<input name='groupElement'type='checkbox' value=${filtered.index}><label for='groupElement'>${filtered.title}</label><br>`;     
+        saveToSyncStorage(group);
       });
     });
+  });
+  alert('got to the end');
+  
+};
+
+function saveToSyncStorage(data, key= "key") {
+  
+  chrome.storage.sync.set({[key]:data}, function(result) {
+    console.log('Saved', key, data);
+  });
+};
+
+function getValue(parameter) {
+
+}
+
+
+function retrieveFromSyncStorage(key) {
+  chrome.storage.sync.get(key, function(result) {
+    return result.key;
   });
 };
 
@@ -36,8 +54,35 @@ function selectedTabIndex() {
 };
 
 function compileNewGroup() {
-  chrome.storage.sync.get('grouping', function(group) {
-    $('#activeGroupList').html(group.grouping);
+  var num = $('.grouping').length;
+
+  saveToSyncStorage(num, "groupCount");
+  
+  test = retrieveFromSyncStorage("groupCount");
+  // chrome.storage.sync.get("groupCount", function(test) {
+  //   console.log(test.value + " was recovered");
+  // });
+  alert(test);
+
+  chrome.storage.sync.get('grouping', function(group) { 
+    var iteratingId = "group" + `${num + 1}`;
+    
+    alert(`num :: ${num}`);
+    console.log('ITERATING ID ::' + iteratingId);
+    if (num == 0) {
+
+      $('#activeGroupList').append(`<div id=${iteratingId} class='grouping'><br><b>Test Dropdown Header</b></br>` + group.grouping + `<br></div>`);  
+    } else if (num >= 1) {
+      $('#' + iteratingId).after(`<div id=${iteratingId} class='grouping'><br><b>Test Dropdown Header</b></br>` + group.grouping + `<br></div>`);  
+
+    };
+  });
+};
+
+function groupTotalCounter(total) {
+  chrome.storage.sync.set({'groupCount':total}, function() {
+    console.log('saved', 'groupCount', total );
+    return total;
   });
 };
 
